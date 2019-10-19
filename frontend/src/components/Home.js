f554a68192a27198c22d08f4bcfd95260bb7d2ae
Bryce Mercines <nplixel@gmail.com>
2019-10-19 08:30:00 +0800

    saving changes for navbar

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Navbar, Nav, InputGroup, Form, FormControl } from 'react-bootstrap';
+
+function Home() {
+  return (
+    <>
+      <Navbar bg="light" expand="lg" className="fixed-top">
+        <Navbar.Brand href="#home">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/4/4b/Libra_logo.svg" className="logo" alt="logo" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+
+          <Form inline>
+            <InputGroup>
+              <FormControl
+                placeholder="Search"
+                aria-label="Username"
+                aria-describedby="search-btn"
+                id="search-bar"
+              />
+              <InputGroup.Prepend>
+                <InputGroup.Text id="search-btn"><i class="fa fa-search"></i></InputGroup.Text>
+              </InputGroup.Prepend>
+            </InputGroup>
+          </Form>
+
+          <Nav className="ml-auto mr-5">
+
+            <Nav.Link href="#home">About</Nav.Link>
+            <Nav.Link href="#link">Community</Nav.Link>
+
+            {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            </NavDropdown> */}
+
+          </Nav>
+
+        </Navbar.Collapse>
+      </Navbar>
+
+
+
+
+    </>
+  );
+}
+
+export default Home;
+
--- frontend/src/components/vanilla.js
+++ frontend/src/components/vanilla.js
@@ -1,1 +1,14 @@
-// vanilla js functions for fun:
+
+export const el = (elem) => {
+  return document.getElementById(elem);
+}
+
+export const isMounted = (elem) => {
+  return document.contains(document.getElementById(elem));
+}
+
+export const sclass = (elemClass) => {
+  return document.getElementsByClassName(elemClass);
+}
+
+
