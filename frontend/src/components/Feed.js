c5f7d90f311bd438d70d6af6f7c46dae83ba354f
brenfreeze <bren.aviador@whitecloak.com>
2019-10-19 15:50:52 +0800

    Added Feed Item

--- frontend/src/components/Feed.js
+++ frontend/src/components/Feed.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import NasaLogo from '../assets/nasa-logo.png'
 
 const Feed = () => {
   return (
@@ -51,15 +51,18 @@ const Feed = () => {
       <div className="feed-content">
         <div className="feed-header">
         </div>
-        <div className="feed-container container">
+        <div className="feed-container">
           {
-            Array.from(Array(1)).map(() => (
+            Array.from(Array(100)).map(() => (
               <div className="feed-item">
                 <div className="feed-details">
-                  <h3 className="hazard-type">Hazard Type</h3>
                   <h1 className="city-name">Sample City Name</h1>
+                  <h3 className="danger-index">Danger Index <span className="index-number">3.2</span></h3>
+                  <div className="sources-images">
+                    <img className="sources-logo" src={NasaLogo} alt=""/>
+                  </div>
                 </div>
-                <img src="https://picsum.photos/300/300" alt=""/>
+                <img className="feed-image" src="https://picsum.photos/300/300" alt=""/>
               </div>
             ))
           }
