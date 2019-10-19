70af7537b28b13dab041a2339c5ad1fe4cf46044
Bryce Mercines <nplixel@gmail.com>
2019-10-19 14:26:19 +0800

    saving changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './css/App.scss';
 import Home from './components/Home';
+import Feed from './components/Feed';
 
 function App() {
   return (
     <div className="App">
       <Home />
+      <Feed />
     </div>
   );
 }
---
+++ frontend/src/components/Feed.js
@@ -0,0 +1,71 @@
+import React from 'react';
+
+
+const Feed = () => {
+  return (
+    <div id="feed">
+      <div className="side-bar">
+        <p><i class="fa fa-bullseye text-danger"></i> Latest Hazard Events</p>
+        <hr />
+
+        <div className="new-feed-card">
+          <span>This is a new feed</span>
+        </div>
+        <div className="new-feed-card">
+          <span>This is a new feed</span>
+        </div> <div className="new-feed-card">
+          <span>This is a new feed</span>
+        </div> <div className="new-feed-card">
+          <span>This is a new feed</span>
+        </div> <div className="new-feed-card">
+          <span>This is a new feed</span>
+        </div> <div className="new-feed-card">
+          <span>This is a new feed</span>
+        </div> <div className="new-feed-card">
+          <span>This is a new feed</span>
+        </div> <div className="new-feed-card">
+          <span>This is a new feed</span>
+        </div> <div className="new-feed-card">
+          <span>This is a new feed</span>
+        </div> <div className="new-feed-card">
+          <span>This is a new feed</span>
+        </div> <div className="new-feed-card">
+          <span>This is a new feed</span>
+        </div> <div className="new-feed-card">
+          <span>This is a new feed</span>
+        </div> <div className="new-feed-card">
+          <span>This is a new feed</span>
+        </div> <div className="new-feed-card">
+          <span>This is a new feed</span>
+        </div> <div className="new-feed-card">
+          <span>This is a new feed</span>
+        </div> <div className="new-feed-card">
+          <span>This is a new feed</span>
+        </div> <div className="new-feed-card">
+          <span>This is a new feed</span>
+        </div> <div className="new-feed-card">
+          <span>This is a new feed</span>
+        </div>
+
+      </div>
+
+
+      <div className="feed-header">
+      </div>
+
+
+
+      <div className="col-md-8">
+
+        <div className="feed-container">
+
+          <h1>Hello</h1>
+
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Feed;
