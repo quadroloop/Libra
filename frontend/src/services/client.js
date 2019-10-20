dfca4862f3507adb29a0440946cca0f87d13b62e
Bryce Mercines <nplixel@gmail.com>
2019-10-20 22:23:23 +0800

    added tips

--- frontend/src/services/client.js
+++ frontend/src/services/client.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 let client = axios.create({
-  baseURL: `https://libra-api-hackathon.herokuapp.com/api`
-  // baseURL: 'http://c7fdf697.ngrok.io/api/'
+  // baseURL: `https://libra-api-hackathon.herokuapp.com/api`
+  baseURL: 'https://57ca02f3.ngrok.io/api'
 })
 
 export default client
